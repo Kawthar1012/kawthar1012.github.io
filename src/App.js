29024bfa32e98a4d9da09b99a3e785505b194f39
Kawthar1012 <k.el@live.fr>
2022-04-13 14:29:31 +0200

    fix language issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Values from './components/pages/Values';
 import Menu from './components/pages/Menu';
