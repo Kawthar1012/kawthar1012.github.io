b319e58ef31b7fee88f7d25a1e8920203c15f983
Kawthar1012 <k.el@live.fr>
2022-04-13 13:23:58 +0200

    fix language issues

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Values from './components/pages/Values';
+import Menu from './components/pages/Menu';
+import Order from './components/pages/Order';
+import Footer from "./components/Footer";
+
+function App() {
+  let languageStoredInLocalStorage = localStorage.getItem("language");
+  let [language, setLanguage] = useState(
+    languageStoredInLocalStorage ? languageStoredInLocalStorage : "English"
+  );
+  return (
+    <>
+      <Router>
+      <Navbar language={language}
+        handleSetLanguage={language => {
+          setLanguage(language);
+          storeLanguageInLocalStorage(language);
+          //window.location.reload(false);
+        }}/>
+      <Routes>
+        <Route path='/' element={<Home language={language}/>}>
+        </Route>
+        <Route path='/values' element={<Values language={language}/>}>
+        </Route>
+        <Route path='/menu' element={<Menu language={language}/>}>
+        </Route>
+        <Route path='/order' element={<Order language={language}/>}>
+        </Route>
+      </Routes>
+      <Footer language={language}/>
+      </Router>
+    </>
+  );
+}
+
+function storeLanguageInLocalStorage(language) {
+  localStorage.setItem("language", language);
+}
+
+export default App;
